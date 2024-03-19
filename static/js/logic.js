e1df965146fead15c96e12c3f549d7bf1d8f1e0c
Patric Beaven <patricbeaven@outlook.com>
2024-03-19 21:05:14 +0800

    remove html, pyscript first file

--- static/js/logic.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('pageSelect').addEventListener('change', function() {
-    var selectedPage = this.value;
-    document.querySelectorAll('.page').forEach(function(page) {
-        page.classList.remove('active');
-        if(page.id === selectedPage) {
-            page.classList.add('active');
-        }
-    });
-});
